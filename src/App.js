da77f36a0384c34bc89c523805a43010f8ad4dc8
Ryuggo <hugo.francois@student.hepl.be>
2022-06-30 10:04:48 +0200

    Spining react so it's not empty

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import SpiningReact from "./components/SpiningReact";
+
 function App() {
   return (
-    <div className="App">
-      <h2>Let's gooo</h2>
-    </div>
+    <SpiningReact />
   );
 }
 
